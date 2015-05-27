3b93aeee06225da2b8ae358feff349ffcfd1d510
Deewendra Shrestha <deewendra.shrestha@nielsen.com>
2015-05-27 15:49:53 -0400

    initial init

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dee',
+    podModulePrefix: 'dee/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
